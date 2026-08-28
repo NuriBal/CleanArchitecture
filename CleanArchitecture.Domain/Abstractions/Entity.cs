dbf141ec8ac13566fe066ef0a8caababd1659770
NuriBal <nnuribal@outlook.com.tr>
2026-08-28 22:02:58 +0300

    Add project files.

---
+++ CleanArchitecture.Domain/Abstractions/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Domain.Abstractions;
+
+public abstract class Entity
+{
+    public string Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ CleanArchitecture.Persistance/Context/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistance.Context;
+
+public sealed class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) {}
+}

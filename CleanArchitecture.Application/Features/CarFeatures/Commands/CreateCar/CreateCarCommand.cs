ee31bf48cc3dcb2ac1351906585988de3cd83aff
NuriBal <nnuribal@outlook.com.tr>
2026-08-28 22:36:48 +0300

    Servisler yazılacak bu kısıma kadar tamamlandı

---
+++ CleanArchitecture.Application/Features/CarFeatures/Commands/CreateCar/CreateCarCommand.cs
@@ -0,0 +1,6 @@
+﻿using CleanArchitecture.Domain.Dtos;
+using MediatR;
+
+namespace CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
+
+public sealed record CreateCarCommand(string Name, string Model, string EnginePower) : IRequest<MessageResponse>;
--- CleanArchitecture.Persistance/Context/AppDbContext.cs
+++ CleanArchitecture.Persistance/Context/AppDbContext.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CleanArchitecture.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Persistance.Context;
 
 public sealed class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entires = ChangeTracker.Entries<Entity>();
+        foreach (var entry in entires)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(p => p.CreatedDate).CurrentValue = DateTime.Now;
+                entry.Property(p => p.IsDeleted).CurrentValue = false;
+            }
+            if (entry.State == EntityState.Modified)
+                entry.Property(p => p.UpdatedDate).CurrentValue = DateTime.Now;
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }

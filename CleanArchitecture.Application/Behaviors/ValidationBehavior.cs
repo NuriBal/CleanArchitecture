013673d1b948077ca06f7dc8bc5ffc458063d056
Nuri BAL <nnuribal@hotmail.com>
2026-08-31 15:32:25 +0300

    Creat işlemi, AutoMapper entegrasyonu, Fluent Validation entegrasyonu. Validasyon ayarları, Oto mapleme ayarları yapıldı

---
+++ CleanArchitecture.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace CleanArchitecture.Application.Behaviors;
+
+public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : class, IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var errorDictionary = _validators
+            .Select(v => v.Validate(context))
+            .SelectMany(result => result.Errors)
+            .Where(failure => failure != null)
+            .GroupBy(
+                failure => failure.PropertyName,
+                failure => failure.ErrorMessage,
+                (propertyName, errorMessages) => new
+                {
+                    Key = propertyName,
+                    Values = errorMessages.Distinct().ToArray()
+                })
+            .ToDictionary(group => group.Key, group => group.Values[0]);
+
+        if (errorDictionary.Any())
+        {
+            var errors = errorDictionary.Select(kvp => new ValidationFailure
+            {
+                PropertyName = kvp.Value,
+                ErrorCode = kvp.Key
+            });
+            throw new ValidationException(errors);
+        }
+
+        return await next();
+    }
+}
---
+++ CleanArchitecture.Application/Features/CarFeatures/Commands/CreateCar/CreateCarCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
+
+public sealed class CreateCarCommandValidator : AbstractValidator<CreateCarCommand>
+{
+    public CreateCarCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı zorunludur.").NotNull().WithMessage("Ad alanı zorunludur.").MaximumLength(10).WithMessage("Ad alanı en az 10 karakter olmalıdır.");
+        RuleFor(x => x.Model).NotEmpty().WithMessage("Model alanı zorunludur.").NotNull().WithMessage("Model alanı zorunludur.").MaximumLength(10).WithMessage("Model alanı en az 10 karakter olmalıdır.");
+        RuleFor(x => x.EnginePower).NotEmpty().WithMessage("Motor gücü alanı zorunludur.").NotNull().WithMessage("Motor gücü alanı zorunludur.").GreaterThan(0).WithMessage("Motor gücü alanı 0'dan büyük olmalıdır.");
+    }
+}
